744d786925e98ca273701e7d052b165734544189
GlenCoffey <glenrcoffey@gmail.com>
2019-01-16 10:08:17 +1100

    Continued work on functionality

--- src/reducers/productsReducer.js
+++ src/reducers/productsReducer.js
@@ -3,8 +3,19 @@ import initialState from './initialState';
 const productsReducer = (state = initialState.productsList, action) => {
     switch(action.type){
         case 'ADD_TO_CART':
-
-      return state;
+        console.log('state', ...state);
+        return  {
+          ...state.map((product) => {
+            if(product.productName === action.productName){
+              return {
+                ...product,
+              inCart:true
+              };
+            }  else {
+              return product;
+            }
+          })
+        };
     default:
       return state;
     }
