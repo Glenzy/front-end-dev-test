06fc54116a5e7adcf4e9886fd7b98dfa2ed89e45
Glen Coffey <glenrcoffey@gmail.com>
2019-01-24 10:52:25 +1100

    Test added to reducer
    A small design update for the active filter in mobile was changed also

--- src/reducers/cartReducer.spec.js
+++ src/reducers/cartReducer.spec.js
@@ -1,11 +1,45 @@
 import cartReducer from './cartReducer';
-import initialState from './initialState';
 
-const originalState = initialState.products;
+const testState = {
+    itemsInCart:0,
+    cartIsOpen:false,
+    productsList: [
+        {
+            "isPublished": "true",
+            "productName": "Apple iPhone X",
+            "productImage": "https://www.telstra.com.au/content/dam/tcom/personal/mobile-phones/product-catalogue/iphone-x/iphone-x-silver-grid.png",
+            "price": "299",
+            "brand": "Apple",
+            "show": true,
+        }
+    ]
+};
+const expectedState = {
+    itemsInCart:1,
+    cartIsOpen:false,
+    productsList: [
+        {
+            "isPublished": "true",
+            "productName": "Apple iPhone X",
+            "productImage": "https://www.telstra.com.au/content/dam/tcom/personal/mobile-phones/product-catalogue/iphone-x/iphone-x-silver-grid.png",
+            "price": "299",
+            "brand": "Apple",
+            "show": false,
+            "inCart":true
+        }
+    ]
+};
 
 describe('The cart reducer', () => {
     it('should set initial state by default', () => {
-        const action = { type: 'unknown' };
-    expect(cartReducer(undefined, action)).toEqual(originalState);
+        const action = { type: 'unknown',};
+    expect(cartReducer(testState, action)).toEqual(testState);
   });
+
+    it('should handle ADD_TO_CART', () => {
+        const productName = 'Apple iPhone X';
+        const action = { type: 'ADD_TO_CART', productName}; 
+        expect(cartReducer(testState, action)).toEqual(expectedState);
+    });
+
 });
